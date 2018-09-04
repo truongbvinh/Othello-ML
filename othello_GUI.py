91cb6a91c5a959352bbf5c226fffc571e10f5196
Vinh Truong <truongbaovinh@gmail.com>
2018-09-03 18:37:47 -0700

    Initial commit

---
+++ othello_GUI.py
@@ -0,0 +1,415 @@
+# Vinh Truong 88812807, lab section 9
+"""
+I know this code is really long, but I did it so that I could keep the
+setup interaction and playing phase in one window without any popups.
+I separated the different methods by the phases of the game.
+"""
+
+import othello_logic
+import tkinter
+
+class othello_GUI:
+	def __init__(self):
+		"""
+		Initializes the board and binds the reconfigure function
+		"""
+		self._othello_game = None
+		self._root_window = tkinter.Tk()
+
+		self._height = 6
+		self._width = 6
+
+		full = tkinter.Label(text = "Full Othello Game", font = 24)
+		full.grid(row = 0, column = 0,
+			pady = 10, sticky = tkinter.W + tkinter.E)
+
+		self._display = tkinter.Canvas(
+			master = self._root_window,
+			width = 75 * self._width,
+			height = 75 * self._height,
+			background = "#D8C68A"
+			)
+
+		self._display.grid(
+			row = 1, column = 0, padx = 30, pady = 0, columnspan = 4,
+			sticky = tkinter.N + tkinter.S + tkinter.W + tkinter.E)
+
+		self.setup_rowcol()
+
+		self._display.bind("<Configure>", self._help_draw)
+
+		self._root_window.rowconfigure(1, weight = 1)
+		self._root_window.columnconfigure(0, weight = 1)
+		self._root_window.rowconfigure(2, weight = 0)
+
+		self._x_pos = 0
+		self._y_pos = 0
+
+	################################# CLICKING FUNCTIONS ##################################
+
+	def run_game(self) -> None:
+		"""
+		Runs the game
+		"""
+		self._root_window.mainloop()
+
+	def _on_canvas_resized(self, event: tkinter.Event) -> None:
+		"""
+		Redraws everything based on fractal values if window is resized
+		"""
+		self._draw_everything()
+
+	def _on_button_press(self, event: tkinter.Event) -> None:
+		"""
+		Makes a move in the Othello logic if a button is pressed
+		"""
+		self._place_tile(self._x_pos, self._y_pos)
+
+
+	def _mouse_position(self, event: tkinter.Event) -> None:
+		"""
+		Records the mouse position
+		"""
+		self._x_pos = event.x
+		self._y_pos = event.y
+
+	def _place_tile(self, x: int, y: int) -> None:
+		"""
+		Places a tile and if it works, then it will redraw the board
+		"""
+		display_height = self._display.winfo_height()
+		display_width = self._display.winfo_width()
+		col = int(x/(display_width / self._width)) + 1
+		row = int(y/(display_height / self._height)) + 1
+		try:
+			self._othello_game.take_turn([row, col])
+			self._draw_circles(self._othello_game.game)
+			self._update_score_and_turn()
+			self.switch_players_or_end()
+		except:
+			pass
+
+	################################ OTHELLO FUNCTIONS #############################
+
+	def has_valid_move(self) -> bool:
+		"""
+		Returns true if the player has a valid move
+		"""
+		is_valid_move = False
+		for row in range(self._othello_game.get_rows()):
+			if is_valid_move:
+				break
+
+			for col in range(self._othello_game.get_cols()):
+
+				if len(self._othello_game.placement_is_valid(row, col)) > 0: #if the move is valid
+					is_valid_move = True
+					break
+
+		return is_valid_move
+
+	def switch_players_or_end(self) -> bool:
+		"""
+		If there are no valid moves for the player, then switch. If none left for anyone, then end game
+		"""
+		if not self.has_valid_move():
+			self._othello_game.change_player()
+			self._update_score_and_turn()
+
+			if not self.has_valid_move():
+				self._display.unbind("<Button-1>")
+				self._display_winner()
+
+	################################ DRAWING FUNCTIONS #############################
+	def _draw_everything(self) -> None:
+		"""
+		Redraws all the canvas objects
+		"""
+		self._display.delete(tkinter.ALL)
+
+		self._draw_board()
+		self._draw_circles(self._othello_game.game)
+
+	def _draw_board(self) -> None:
+		"""
+		Draws the grid to place the pieces on
+		"""
+		display_width = self._display.winfo_width()
+		display_height = self._display.winfo_height()
+		
+		for vertical in range(1, self._width + 1):
+			self._display.create_line(
+				(vertical / self._width) * display_width, 
+				0,
+				(vertical / self._width) * display_width, 
+				display_height,
+				fill = "#000000")
+
+		for horiz in range(1, self._height + 1):
+			self._display.create_line(
+				0, 
+				(horiz / self._height) * display_height, 
+				display_width, 
+				(horiz / self._height) * display_height,
+				fill = "#000000")
+
+	def _draw_circles(self, board: [[int]]) -> None:
+		"""
+		Draws the pieces on the board
+		"""
+		for row in range(self._height):
+			for col in range(self._width):
+				if self._othello_game.game[row][col] != 0:
+					self._draw_piece(row, col, self._othello_game.game[row][col])
+
+
+	def _draw_piece(self, row: int, col: int, color: int) -> None:
+		"""
+		Draws the peices based on the player's color
+		"""
+		display_width = self._display.winfo_width()
+		display_height = self._display.winfo_height()
+		h_padding = (display_height / self._height) / 15
+		w_padding = (display_width / self._width) / 15
+		row += 1
+		col += 1
+
+		if color == 2:
+			filler = "white"
+		elif color == 1:
+			filler = "black"
+		self._display.create_oval(
+			(col / self._width) * display_width - (1/self._width) * display_width + w_padding,
+			(row / self._height) * display_height - (1/self._height) * display_height + h_padding, 
+			(col / self._width) * display_width - w_padding,
+			(row / self._height) * display_height - h_padding, 
+			outline = filler, fill = filler)
+
+
+	############################# INITIATE PHASE 1 SETUP BOARD SETTINGS ############################
+
+	def setup_rowcol(self) -> None:
+		"""
+		Creates the dropdown menus to allow the player to choose the
+		amount of rows and cols, the player, and the wintpe
+		"""
+		self._settings_frame = tkinter.Frame(master = self._root_window)
+		self._settings_frame.grid(
+			row = 2, column = 0, rowspan = 2, columnspan = 5, padx = 10, pady = 10,
+			sticky = tkinter.W + tkinter.E)
+
+		self._rows_setter = tkinter.IntVar(self._settings_frame)
+		self._rows_setter.set(6) # default value
+		self._cols_setter = tkinter.IntVar(self._settings_frame)
+		self._cols_setter.set(6)
+		self._first_setter = tkinter.StringVar(self._settings_frame)
+		self._first_setter.set("Black")
+		self._wintype_setter = tkinter.StringVar(self._settings_frame)
+		self._wintype_setter.set("Most")
+		self._settings_done = tkinter.Button(self._settings_frame, text = "Done", font = 30,
+			command = self._create_board)
+
+		tkinter.Label(self._settings_frame, text = "Rows").grid(row = 0, column = 0, padx = 10, sticky = tkinter.S)
+		_row_dropdown = tkinter.OptionMenu(self._settings_frame, 
+			self._rows_setter, 4, 6, 8, 10, 12, 14, 16)
+		_row_dropdown.grid(row = 1, column = 0,
+			padx = 10, pady = 10)
+		tkinter.Label(self._settings_frame, text = "Columns").grid(row = 0, column = 1, padx = 10, sticky = tkinter.S)
+		_col_dropdown = tkinter.OptionMenu(self._settings_frame, 
+			self._cols_setter, 4, 6, 8, 10, 12, 14, 16)
+		_col_dropdown.grid(row = 1, column = 1,
+			padx = 10, pady = 10)
+		tkinter.Label(self._settings_frame, text = "First Player").grid(row = 0, column = 2, padx = 10, sticky = tkinter.S)
+		_first_dropdown = tkinter.OptionMenu(self._settings_frame,
+			self._first_setter, "Black", "White")
+		_first_dropdown.grid(row = 1, column = 2,
+			padx = 10, pady = 10)
+		tkinter.Label(self._settings_frame, text = "Win Condition").grid(row = 0, column = 3, padx = 10, sticky = tkinter.S)
+		_wintype_dropdown = tkinter.OptionMenu(self._settings_frame,
+			self._wintype_setter, "Most", "Least")
+		_wintype_dropdown.grid(row = 1, column = 3,
+			padx = 10, pady = 10)
+		self._settings_done.grid(row = 1, column = 4,
+			padx = 10, pady = 10)
+		self._rows_setter.trace("w", self._help_draw)
+		self._cols_setter.trace("w", self._help_draw)
+
+		for i in range(5):
+			self._settings_frame.columnconfigure(i, weight = 1)
+
+	def _help_draw(self, *args) -> None:
+		"""
+		Draws only the board without the pieces
+		"""
+		self._height = self._rows_setter.get()
+		self._width = self._cols_setter.get()
+		self._display.delete(tkinter.ALL)
+		self._draw_board()
+
+	def _set_first(self) -> str:
+		"""
+		Sets the first player based on the value of the dropdown menu
+		"""
+		if self._first_setter.get() == "Black":
+			return "B"
+		return "W"
+
+	def _set_winstyle(self) -> str:
+		"""
+		Sets the winstyle based on what the user chose on the dropdown menu
+		"""
+		if self._wintype_setter.get() == "Most":
+			return ">"
+		return "<"
+
+	def _create_board(self) -> None:
+		"""
+		Rebinds keys and goes on to next phase which allows player to set peices
+		"""
+		board = []
+		for i in range(self._height):
+			board.append([0] * self._width)
+
+		self._othello_game = othello_logic.Othello(self._rows_setter.get(), 
+			self._cols_setter.get(), self._set_first(), self._set_winstyle(),
+			board)
+
+		self._display.bind("<Motion>", self._mouse_position)
+		self._display.bind("<Button-1>", self._on_button_press_setup)
+		self._display.bind("<Configure>", self._on_canvas_resized)
+		self._settings_frame.destroy()
+		self._setup_pieces()
+
+
+	###################################### PHASE 2 SETUP PIECES ######################################
+	def _black_pressed(self) -> None:
+		"""
+		Presses the button down and raises the other if user presses Black
+		"""
+		self._black_setup["relief"] = "sunken"
+		self._white_setup["relief"] = "raised"
+
+	def _white_pressed(self) -> None:
+		"""
+		Pressed the button down and raises the other if user presses White
+		"""
+		self._white_setup["relief"] = "sunken"
+		self._black_setup["relief"] = "raised"
+
+	def _clear_pieces(self) -> None:
+		"""
+		Function for if the user pressed the "Clear" button, clears the pieces and redraws
+		"""
+		for row in range(self._height):
+			for col in range(self._width):
+				self._othello_game.game[row][col] = 0
+		self._draw_everything()
+
+	def _setup_pieces(self) -> None:
+		"""
+		Sets up the frame for the user to use to choose chich pieces to set up, to clear
+		the board, or to continue on to playing the game
+		"""
+		self._settings_frame = tkinter.Frame(master = self._root_window)
+		self._settings_frame.grid(
+			row = 2, column = 0, columnspan = 3, padx = 10, pady = 10,
+			sticky = tkinter.W + tkinter.E)
+		self._settings_frame.columnconfigure(3, weight = 1)
+
+		self._black_setup = tkinter.Button(self._settings_frame, text = "Black", font = 30, 
+			command = self._black_pressed, relief = "sunken")
+		self._white_setup = tkinter.Button(self._settings_frame, text = "White", font = 30,
+			command = self._white_pressed)
+		self._clear_pieces = tkinter.Button(self._settings_frame, text = "Clear", font = 30,
+			command = self._clear_pieces)
+		self._settings_done = tkinter.Button(self._settings_frame, text = "Done", font = 30,
+			command = self._start_game)
+		self._black_setup.grid(row = 0, column = 0, padx = 10, pady = 10)
+		self._white_setup.grid(row = 0, column = 1, padx = 10, pady = 10)
+		self._clear_pieces.grid(row = 0, column = 2, padx = 30, pady = 10)
+		self._settings_done.grid(row = 0, column = 3, padx = 10, pady = 10, sticky = tkinter.E)
+
+	def _on_button_press_setup(self, event: tkinter.Event) -> None:
+		"""
+		For when the uses presses the button during the setting phase. Gets which button is
+		sunken and places that piece down when mouse is clicked
+		"""
+		display_height = self._display.winfo_height()
+		display_width = self._display.winfo_width()
+		col = int(self._x_pos/(display_width / self._width))
+		row = int(self._y_pos/(display_height / self._height))
+
+		if self._black_setup["relief"] == "sunken":
+			if self._othello_game.game[row][col] == 1:
+				self._othello_game.game[row][col] = 0
+			else:
+				self._othello_game.game[row][col] = 1
+		else:
+			if self._othello_game.game[row][col] == 2:
+				self._othello_game.game[row][col] = 0
+			else:
+				self._othello_game.game[row][col] = 2
+		self._draw_everything()
+
+	def _start_game(self) -> None:
+		"""
+		Used when the user presses the "Done" button, destroys the frame and goes onto next phase
+		"""
+		self._settings_frame.destroy()
+		self._display.bind("<Button-1>", self._on_button_press)
+		self._setup_display()
+		self.switch_players_or_end()
+
+	############################### GAME STARTED PHASE 3 #####################################
+	def _setup_display(self) -> None:
+		"""
+		Sets up the display for the score to be displayed
+		"""
+		self._settings_frame = tkinter.Frame(master = self._root_window)
+		self._settings_frame.grid(
+			row = 2, column = 0, columnspan = 3, padx = 10, pady = 10, sticky = tkinter.W + tkinter.E)
+		self._score_and_turn_display()		
+
+	def _score_and_turn_display(self) -> None:
+		"""
+		Displays the black and white scores
+		"""
+		black_pieces, white_pieces = self._othello_game._count_pieces()
+		current_player = self._othello_game.get_current_player()
+
+		self.black_label = tkinter.Label(self._settings_frame, text = "Black: {}".format(black_pieces),
+			font = 30)
+		self.white_label = tkinter.Label(self._settings_frame, text = "White: {}".format(white_pieces),
+			font = 30)
+		self.turn_label = tkinter.Label(self._settings_frame, text = "Turn: {}".format(current_player),
+			font = 30)
+		self.black_label.grid(row = 0, column = 0, padx = 10, pady = 10)
+		self.white_label.grid(row = 0, column = 1, padx = 10, pady = 10)
+		self.turn_label.grid(row = 0, column = 2, padx = 10, pady = 10)
+
+	def _update_score_and_turn(self) -> None:
+		"""
+		Updates the score when a button is pressed
+		"""
+		black_pieces, white_pieces = self._othello_game._count_pieces()
+
+		self.black_label["text"] = "Black: {}".format(black_pieces)
+		self.white_label["text"] = "White: {}".format(white_pieces)
+		self.turn_label["text"] = "Turn: {}".format(self._othello_game.get_current_player())
+
+	############################## WINNING WINDOW WINNERS ONLY ###############################
+
+	def _display_winner(self) -> None:
+		"""
+		Displays the winner of the game
+		"""
+		self.turn_label.destroy()
+		winner_label = tkinter.Label(self._settings_frame, 
+			text = "WINNER: {}!".format(self._othello_game.find_winner()), font = 30)
+		winner_label.grid(row = 0, column = 2, padx = 10, pady = 10)
+
+
+
+if __name__ == "__main__":
+	test = othello_GUI()
+	test.run_game()
---
+++ othello_logic.py
@@ -0,0 +1,362 @@
+# Vinh Truong 88812807, Lab Section 9
+
+class InvalidMoveError(Exception):
+	pass
+
+class GameFinishedError(Exception):
+	pass
+
+class InvalidSizeError(Exception):
+	pass
+
+class InvalidValueError(Exception):
+	pass
+
+class InvalidBoardError(Exception):
+	def __str__(self) -> str:
+		return "Board has to be even dimensions from 4 to 16"
+
+class Othello:
+	"""
+	ATTRIBUTES:
+	REFER, for program reference
+	FULL_OR_SIM, full or simple game
+	game, gameboard
+	ROWS, number of rows
+	COLS, number of cols
+	current_player, current player (1 or 2)
+	"""
+
+	REFER = {0: ".", 1: "B", 2: "W"}
+	FULL_OR_SIM = "FULL"
+
+	#################### INIT FUNCTIONS ###############################
+	def __init__(self, rows: int, cols: int, first: str, style: str, board: [str]):
+		"""
+		Initializes the board, first player, win condition based on the imput
+		"""
+		self._check_row_col(rows, cols)
+		self._check_first(first)
+		self._check_win_type(style)
+		self.game = self._create_board(rows, cols)
+		self.ROWS = rows
+		self.COLS = cols
+		self.current_player = self._first_player(first)
+		self.STYLE = style
+		self._set_board(board)
+
+
+	def _create_board(self, rows: int, cols: int) -> [[int]]:
+		"""
+		Creates a board based on user input. For reference,
+		0 is empty, 1 is Black, 2 is White
+		"""
+		board = []
+
+		if rows < 4 or rows > 16 or cols < 4 or  cols > 16:
+			raise InvalidBoardError
+		elif rows % 2 == 1 or cols % 2 == 1:
+			raise InvalidBoardError
+
+		for i in range(rows):
+			board.append([0]*cols)
+
+		return board
+
+	def _check_row_col(self, row: int, col: int):
+		"""
+		Checks if the row and column fits acceptable values. Raises error if not
+		"""
+		if row % 2 == 1 or col % 2 == 1:
+			raise InvalidSizeError
+		if row < 4 or row > 16 or col < 4 or col > 16:
+			raise InvalidSizeError
+
+	def _check_win_type(self, win: str):
+		"""
+		Checks if the win type fits acceptable values. Raises error if not
+		"""
+		if win != ">" and win != "<":
+			raise InvalidValueError
+
+	def _check_first(self, player: str):
+		"""
+		Checks if the first player fits acceptable values. Raises error if not
+		"""
+		if player != "B" and player != "W":
+			raise InvalidValueError
+
+
+	def _first_player(self, first: str) -> str:
+		"""
+		Returns the first player, "B" will return 1, "W" returns 2
+		"""
+		if first == "B":
+			return 1
+		elif first == "W":
+			return 2
+
+
+	def _set_board(self, board: [[str]]) -> None:
+		"""
+		Sets the object's board based on a string which will represent the
+		board
+		"""
+		if len(board) > len(self.game):
+			raise InvalidSizeError
+
+		for row in range(len(self.game)):
+
+			if len(board[row]) > len(self.game[row]):
+				raise InvalidSizeError
+
+			for col in range(len(self.game[row])):
+				if board[row][col] == ".":
+					self.game[row][col] = 0
+				elif board[row][col] == "B":
+					self.game[row][col] = 1
+				elif board[row][col] == "W":
+					self.game[row][col] = 2
+
+
+	def print_board(self) -> None:
+		"""
+		Takes in a game board and prints it out to console
+		"""
+		b = 0
+		w = 0
+		result = ""
+		for row in self.game:
+			for col in row:
+				if col == 1:
+					b += 1
+				elif col == 2:
+					w += 1
+				result += self.REFER[col] + " "
+			result = result.strip()
+			result += "\n"
+		print(result, end = "")
+
+
+	############################ GET METHODS ###############################
+	def get_black_pieces(self) -> int:
+		"""
+		Returns the number of black pieces
+		"""
+		total = 0
+		for row in self.game:
+			for col in row:
+				if col == 1:
+					total += 1
+
+		return total
+
+	def get_white_pieces(self) -> int:
+		"""
+		Returns the number of white pieces
+		"""
+		total = 0
+		for row in self.game:
+			for col in row:
+				if col == 2:
+					total += 1
+
+		return total
+
+	def get_rows(self) -> int:
+		"""
+		Returns the number of rows in the board
+		"""
+		return self.ROWS
+
+	def get_cols(self) -> int:
+		"""
+		Returns the number of cols in the board
+		"""
+		return self.COLS
+
+	def get_current_player(self) -> int:
+		"""
+		Returns the current player:
+		1 is Black
+		2 is White
+		"""
+		if self.current_player == 1:
+			return "Black"
+		return "White"
+
+	############################# MOVE VALIDATION ##########################
+
+	def _direction_is_valid(self, row, col, dir1, dir2) -> ["tuples of coordinate"]:
+		"""
+		Returns a list of tuples containing all the tiles to be flipped in
+		given direction based on the current player, returns empty list if
+		direction is not valid
+		"""
+		tiles = []
+		has_between = False
+
+		row += dir1
+		col += dir2
+
+		while (row < self.ROWS and row >= 0) and (col < self.COLS and col >= 0):
+
+			if self.game[row][col] == self.current_player:
+				if has_between:
+					break
+				else:
+					return []
+			elif self.game[row][col] == 0:
+				return []
+			else:
+				has_between = True
+				tiles.append((row, col))
+
+			row += dir1
+			col += dir2
+
+		if (row > self.ROWS-1 or row < 0) or (col > self.COLS-1 or col < 0):
+			# I need this because there is a case when rows or cols get to 0, appends
+			# a piece and thus doesn't break out of the loop UNTIL either rows or
+			# cols reach a negative number, which would still be a valid index.
+			# This is okay since iif it were a valid move, the while loop would have
+			# been broken out of before they would be negative
+			return []
+
+		if has_between:
+			return tiles
+		else:
+			return []
+
+
+	def placement_is_valid(self, row, col) -> ["tuples of coordinates"]:
+		"""
+		Returns a list of tuples containing the coordinates of the tiles
+		to be flipped, returns empty list if the move is not valid
+		"""
+		if (self.game[row][col] != 0 or 
+			(row > self.ROWS - 1 or row < 0) or 
+			(col > self.COLS - 1 or col < 0)):
+			return []
+			# returns empty if the index is invalid
+
+		result = []
+		for dir1 in range(-1, 2):
+			for dir2 in range(-1, 2):
+				if dir1 == 0 and dir2 == 0:
+					continue
+				try:
+					result.extend(self._direction_is_valid(row, col, dir1, dir2))
+				except IndexError:
+					pass
+		return result
+
+
+	def take_turn(self, move: list) -> None:
+		"""
+		Places a piece from the current player. If the move is invalid,
+		prints INVALID and takes in another coordinate until it is valid, then
+		switches players
+		"""
+
+		if self.board_is_full():
+			raise GameFinishedError
+
+		move[0] = (int)(move[0]) - 1
+		move[1] = (int)(move[1]) - 1 # user enters the first row as 1, not 0
+
+		self._make_move(move[0], move[1])
+
+		self.change_player()
+
+
+
+	def _make_move(self, row, col) -> bool:
+		"""
+		Makes a move for the current player, returns true if
+		a move was made, false if not
+		"""
+		try:
+			moves = self.placement_is_valid(row, col)
+		except IndexError:
+			raise InvalidMoveError
+
+		if len(moves) == 0:
+			raise InvalidMoveError
+
+		self.game[row][col] = self.current_player
+
+		for move in moves:
+			if self.game[move[0]][move[1]] == 1:
+				self.game[move[0]][move[1]] = 2
+			else:
+				self.game[move[0]][move[1]] = 1
+
+		return True
+
+
+
+	############################## PLAYING FUNCTIONS ##########################
+
+
+	def board_is_full(self) -> bool:
+		"""
+		Method to check if the board is full, returns True if it is, False if not
+		"""
+		for row in self.game:
+			for col in row:
+				if col == 0:
+					return False
+		return True
+
+
+	def find_winner(self) -> str:
+		"""
+		Finds the winnder of the game. Returns None if the game is not
+		done yet, else it returns a string containing either "B", "W" or
+		"NONE"
+		"""
+		pieces = self._count_pieces()
+		b = pieces[0]
+		w = pieces[1]
+
+		if self.STYLE == ">":
+			if b > w:
+				return "Black"
+			elif w > b:
+				return "White"
+			else:
+				return "NONE"
+		else:
+			if b < w:
+				return "Black"
+			elif w < b:
+				return "Wwhite"
+			else:
+				return "NONE"
+
+	def _count_pieces(self) -> "tuple of pieces, (black, white)":
+		"""
+		Helper method to count the pieces in the board
+		"""
+		b = 0
+		w = 0
+
+		for row in self.game:
+			for col in row:
+				if col == 1:
+					b += 1
+				elif col == 2:
+					w += 1
+
+		return (b, w)
+
+
+	def change_player(self) -> None:
+		"""
+		changes to the next player's turn
+		"""
+		if self.current_player == 1:
+			self.current_player = 2
+		elif self.current_player == 2:
+			self.current_player = 1
